2825ebf5687032c664e1d5dd9f2a389b41da19d8
FortInsight <fortinsightanalytics@gmail.com>
2026-05-01 11:34:17 -0600

    Update Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-window.SUPABASE_URL = "";
-window.SUPABASE_KEY = "";
+window.SUPABASE_URL ="https://atdrcseaofseybsorhhr.supabase.co";
+window.SUPABASE_KEY = "sb_publishable_buo26QzG4HoNSL2Q03etaw_B4H4DvO8";
+
